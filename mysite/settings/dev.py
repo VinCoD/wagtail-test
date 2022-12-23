5785dfc3cc0da0ee95d3d13b44f0552c68577f59
VinCoD <5895.2020@students.ku.ac.ke>
2022-12-23 20:22:14 +0300

    Initial commit

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-n_g4sh_zbyh-c!#ue@@+^qzld#rtn&_c9(*c_(u#kmmpsc7g)f"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
