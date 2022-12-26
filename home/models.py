68373813098bed6c0f03d3e5926ad4ea266c8977
VinCoD <5895.2020@students.ku.ac.ke>
2022-12-26 12:24:54 +0300

    Added home page

--- home/models.py
+++ home/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
 
 from wagtail.models import Page
 
 
 class HomePage(Page):
-    pass
+    body = RichTextField(blank=True)
+    content_panels = Page.content_panels + [
+        FieldPanel('body'),
+    ]
